81be695b3c1e0037744378d2eb1069be3ed7076e
Md. Nafiul Alam Shishir <shishirght29@gmail.com>
2026-03-15 22:15:51 +0600

    cashout option is now functional

---
+++ cashout.js
@@ -0,0 +1,78 @@
+document.getElementById('cashout-btn').addEventListener('click',function(){
+    // cashout agent number
+    const cashoutInput = getInput('cashout-number')
+    // agent number validation
+    if(cashoutInput.length !=11)
+    {
+        alert("Please Enter 11 Digit Agent Number")
+        return
+    }
+    // cashout input amount
+    const cashoutInputAmount = getInput('cashout-amount')
+    // current balance
+    const balanceElement = document.getElementById('current-balance')
+    const balance = balanceElement.innerText;
+    // pin validation
+    const pin = getInput('cashout-pin')
+    if (pin !== '2126') {
+        alert("Invalid Pin")
+        return;
+    }
+
+    // balance calculator
+    const newBalance = Number(balance) - Number(cashoutInputAmount)
+   
+
+    if (newBalance < 0) {
+        alert("Invalid Amount")
+        return;
+    }
+
+    balanceElement.innerText = newBalance;
+    alert("Cashout Successful")
+})
+
+
+
+// document.getElementById('cashout-btn').addEventListener('click', function () {
+
+//     // cashout agent number
+//     const cashOutInputNumber = document.getElementById('cashout-number')
+//     const cashoutInput = cashOutInputNumber.value;
+//     if(cashoutInput.length !=11)
+//     {
+//         alert("Please Enter 11 Digit Agent Number")
+//         return
+//     }
+
+//     // cashout amount
+//     const cashoutInputAmount = document.getElementById('cashout-amount')
+//     const cashoutAmount = cashoutInputAmount.value;
+//     console.log(cashoutAmount)
+
+//     // current balance
+//     const balanceElement = document.getElementById('current-balance')
+//     const balance = balanceElement.innerText;
+//     console.log(balance)
+
+//     // pin
+//     const cashOutPinInput = document.getElementById('cashout-pin')
+//     const cashoutPin = cashOutPinInput.value;
+
+//     if (cashoutPin !== '2126') {
+//         alert("Invalid Pin")
+//         return;
+//     }
+
+//     // balance calculator
+//     const newBalance = Number(balance) - Number(cashoutAmount)
+
+//     if (newBalance < 0) {
+//         alert("Invalid Amount")
+//         return;
+//     }
+
+//     balanceElement.innerText = newBalance;
+//     alert("Cashout Successful")
+
+// })
---
+++ machine.js
@@ -0,0 +1,8 @@
+
+// function for get input value
+
+function getInput(id){
+    const input = document.getElementById(id)
+    const value = input.value;
+    return value;
+}
